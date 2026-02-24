08149a107323447be6a17bbf03f1687f539ae531
soumitra-soumi <40395441+soumitra-soumi@users.noreply.github.com>
2026-02-23 19:38:55 -0500

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'soumi'
+SITENAME = 'soumitra-soumi'
+SITEURL = ""
+
+TIMEZONE = 'Europe/Rome'
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PATH = 'C:\\Users\\soumi\\blog\\content\\'
+
+THEME = 'C:\\Users\\soumi\\blog\\theme'
+
+
+# Social widget
+SOCIAL = (
+    ("LinkedIn", "#"),
+)
+
+DEFAULT_PAGINATION = 20
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
